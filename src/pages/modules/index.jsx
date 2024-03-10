c3c2de107257a15277941ec3ccbc331648fd9d39
Vitali Karpuk <vitalikkarpuk652@gmail.com>
2024-03-10 14:18:38 +0300

    add

--- src/pages/modules/index.jsx
+++ src/pages/modules/index.jsx
@@ -7,26 +7,17 @@ import Button from "../../components/atoms/button";
 const Home = () => {
   return (
     <>
-      <div className="container" style={{flexGrow: 1}}>
+      <div className="container" style={{ flexGrow: 1 }}>
         <div className={style.initModule}>
-          <Video link="https://youtu.be/wM7QK1v7iqk" />
+          <Video link="https://youtu.be/8mbjQ0H7Q5U" />
           <Link
             to={
-              "https://pay.raschet.by/?param=23695-3-133#00020101021132460010by.raschet01074440631101123695-3-13312021233440014by.epos.artpay0305236950401305011060313353039335402495802BY5918IP%20Kushnerova%20D.V.6008Luninets6304CBC9"
+              "https://docs.google.com/forms/d/e/1FAIpQLSeifCxZg3TOYUceGHvoYSTDsk3ItvOZG5Ll-TJWFEEQynBu-w/viewform?usp=sharing"
             }
             className={style.button}
             target="_blank"
           >
-            <Button text="Оплата интенсива ФОРМУЛА УСПЕХА" variant={'red'}/>
-          </Link>
-          <Link
-            to={
-              "https://www.instagram.com/daria_kushnerova.psy?igsh=ZWdtM2M3Zjc0MnBs"
-            }
-            className={style.button}
-            target="_blank"
-          >
-            <Button text={"Написать мне в инстаграм"} />
+            <Button text="Запись на бесплатный разбор" variant={"red"} />
           </Link>
         </div>
       </div>
